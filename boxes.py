4d2a0209872e12486eeaa556f0ea79b7d74fc7e1
Bilal Qadar <qadarbilal@gmail.com>
2019-08-12 17:15:18 -0400

    Added main call

--- boxes.py
+++ boxes.py
@@ -1,37 +1,38 @@
 import cv2
 import numpy as np
 from contours import *
+
 def box_extraction(img_for_box_extraction_path, cropped_dir_path):
     img = cv2.imread(img_for_box_extraction_path, 0)  # Read the image
     (thresh, img_bin) = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)  # Thresholding the image
     img_bin = 255-img_bin  # Invert the image
     cv2.imwrite("Image_bin.jpg",img_bin)
-       
+
         # Defining a kernel length
     kernel_length = np.array(img).shape[1]//100
-     
+
     # A verticle kernel of (1 X kernel_length), which will detect all the verticle lines from the image.
     verticle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
     # A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
     hori_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
     # A kernel of (3 X 3) ones.
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-# Morphological operation to detect verticle lines from an image
+    # Morphological operation to detect verticle lines from an image
     img_temp1 = cv2.erode(img_bin, verticle_kernel, iterations=3)
     verticle_lines_img = cv2.dilate(img_temp1, verticle_kernel, iterations=3)
     cv2.imwrite("verticle_lines.jpg",verticle_lines_img)
-# Morphological operation to detect horizontal lines from an image
+    # Morphological operation to detect horizontal lines from an image
     img_temp2 = cv2.erode(img_bin, hori_kernel, iterations=3)
     horizontal_lines_img = cv2.dilate(img_temp2, hori_kernel, iterations=3)
     cv2.imwrite("horizontal_lines.jpg",horizontal_lines_img)
-# Weighting parameters, this will decide the quantity of an image to be added to make a new image.
+    # Weighting parameters, this will decide the quantity of an image to be added to make a new image.
     alpha = 0.5
     beta = 1.0 - alpha
     # This function helps to add two image with specific weight parameter to get a third image as summation of two image.
     img_final_bin = cv2.addWeighted(verticle_lines_img, alpha, horizontal_lines_img, beta, 0.0)
     img_final_bin = cv2.erode(~img_final_bin, kernel, iterations=2)
     (thresh, img_final_bin) = cv2.threshold(img_final_bin, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-# For Debugging
+    # For Debugging
     # Enable this line to see verticle and horizontal lines in the image which is used to find boxes
     cv2.imwrite("img_final_bin.jpg",img_final_bin)
     # Find contours for image, which will detect all the boxes
@@ -42,7 +43,7 @@ def box_extraction(img_for_box_extraction_path, cropped_dir_path):
         for c in contours:
         # Returns the location and width,height for every contour
             x, y, w, h = cv2.boundingRect(c)
-# If the box height is greater then 20, widht is >80, then only save it as a box in "cropped/" folder.
+            # If the box height is greater then 20, widht is >80, then only save it as a box in "cropped/" folder.
             if (w < 70 and h < 70) and abs(w-h)<7:
                 idx += 1
                 new_img = img[y:y+h, x:x+w]
@@ -50,5 +51,5 @@ def box_extraction(img_for_box_extraction_path, cropped_dir_path):
     else:
         print('Sorry No contour Found.')
 
-
-box_extraction("sheet.JPG", "./Cropped/")
+if __name__ == "__main__":
+    box_extraction("sheet.JPG", "./Cropped/")
