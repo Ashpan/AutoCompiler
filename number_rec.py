793f0778d7135db7a329d846cb8f0b01f901c297
Bilal Qadar <qadarbilal@gmail.com>
2019-09-19 13:58:06 -0400

    Moved test method to its own file

--- number_rec.py
+++ number_rec.py
@@ -89,30 +89,6 @@ def number_recognition(file_name):
     else:
         return [None]
 
-def test_data(directory):
-    images = os.listdir(directory)
-    size = len(images)
-    individual_acc = {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
-    correct = 0
-    incorrect = 0
-    for i in range(len(images)):
-        guess = number_recognition(os.path.join(directory,images[i]))
-
-        if guess[0] == images[i][0]:
-            correct += 1
-            individual_acc[int(images[i][0])] += 1
-        else:
-            incorrect += 1
-        print("Guess for " + images[i] + ": " + str(guess[0]))
-
-    accuracy = (correct/size)*100
-    for key in individual_acc.keys():
-        ind_acc = (individual_acc[key]/(size/10))*100
-        print(str(key) + " Accuracy: " + str(ind_acc))
-
-    print("Overall Accuracy: " + str(accuracy))
-    print("Test Set Size: " + str(size))
-
 def convert_png(file_path):
     file_name, file_extenstion = os.path.splitext(file_path)
 
@@ -124,8 +100,3 @@ def convert_png(file_path):
         return file_name + ".png"
     else:
         raise TypeError("Image must be a .jpg or .png file")
-
-if __name__ == "__main__":
-    number = test_data("/Users/bilalqadar/Documents/GitHub/FuckCompiling/test_data/pencil_set")
-    print(number)
-    #pre("/Users/bilalqadar/Documents/GitHub/FuckCompiling/test_data/pencil_set/0_1.png")
